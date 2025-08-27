8131425dc1fb5928f26d3ebd1828fb61fad42276
Avishka Chamod <avishkachamod14@gmail.com>
2025-08-27 18:04:57 +0530

    password recover completed

--- src/java/model/Util.java
+++ src/java/model/Util.java
@@ -1,6 +1,8 @@
 
 package model;
 
+import java.util.Random;
+
 public class Util {
     
      public static boolean isEmailValid(String email){
@@ -11,5 +13,14 @@ public class Util {
        return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$");
    }
       
+   public static String generateCode(){
+       Random random = new Random();
+       StringBuilder sb = new StringBuilder();
+       for(int i = 0; i < 4 ; i++){
+           sb.append(random.nextInt(10));
+       }
+       return sb.toString();
+   }
+   
     
 }
