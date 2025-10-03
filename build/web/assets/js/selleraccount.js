67e5a6e111a57099302d37ff6c3724d4c17b8ef1
Avishka Chamod <avishkachamod14@gmail.com>
2025-10-03 09:40:32 +0530

    seller account back end loader completed

--- build/web/assets/js/selleraccount.js
+++ build/web/assets/js/selleraccount.js
@@ -59,3 +59,161 @@ messageInput.addEventListener("keypress", e => {
     sendMessage();
   }
 });
+
+async function sendMail() {
+
+    const data = {
+        email: document.getElementById("email-seller").value
+    };
+
+    const mailAddress = JSON.stringify(data);
+
+    console.log(mailAddress);
+
+    const response = await fetch("http://localhost:8080/InkSpire/MailServlet", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: mailAddress
+    });
+
+    if (response.ok) {
+        const result = await response.json();
+
+        if (result.status) {
+
+            $(".grey-button").notify("Code Has send to your email!", "success");
+
+        } else {
+            $(".grey-button").notify("Invalid E-Mail Address", "error");
+        }
+    } else {
+        $(".grey-button").notify("Something went wrong Please try again Later", "error");
+    }
+
+}
+
+async function verifyCode() {
+
+
+    const userCode = document.getElementById("code").value;
+
+    console.log(userCode);
+
+    const response = await fetch("http://localhost:8080/InkSpire/VerifyCodeServlet", {
+
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: "userCode=" + encodeURIComponent(userCode),
+        credentials: "include"
+    });
+
+    const result = await response.text();
+
+    if (result === "Verification successful!") {
+        document.getElementById("get-code").style.display = "none";
+        document.getElementById("seller-activate-section").style.display = "block";
+
+    } else {
+        $(".btncode").notify("Verification Code is Invalid !", "error");
+    }
+
+
+}
+
+async function activateSellerAccount() {
+
+    let userId = document.cookie
+            .split("; ")
+            .find(row => row.startsWith("userId"))
+            ?.split("=")[1];
+
+    if (userId) {
+
+        const data = JSON.stringify({
+            username: document.getElementById("username").value
+        });
+
+        const response = await fetch(
+                `http://localhost:8080/InkSpire/VerifySellerAccount?id=${encodeURIComponent(userId)}`,
+                {
+                    method: "POST",
+                    body: data,
+                    header: {
+                        "Content-Type": "application/json"
+                    }
+                }
+
+        );
+
+        if (response.ok) {
+
+            const json = await response.json();
+
+            if (json.status) {
+                $(".btnac").notify("Seller Account Acivated Successfully", "success");
+
+            } else {
+                $(".btnac").notify(json.message, "error");
+            }
+
+
+        } else {
+            $(".btnac").notify("Network Error Please try again Later", "error");
+        }
+
+    } else {
+        $(".btnac").notify("Please Log In Again!", "error");
+    }
+
+
+}
+
+
+async function loadSellerInfo() {
+
+
+    let userId = document.cookie
+            .split("; ")
+            .find(row => row.startsWith("userId"))
+            ?.split("=")[1];
+
+    if (userId) {
+
+        const response = await fetch(`http://localhost:8080/InkSpire/LoadSellerBaseInfo?id=${encodeURIComponent(userId)}`);
+
+        if (response.ok) {
+
+            const json = await response.json();
+
+            if (json.status) {
+
+                if (json.seller) {
+                    console.log(json.seller);
+                    document.getElementById("account-layout").style.display = "flex";
+                    document.getElementById("seller-profile-not-layout").style.display = "none";
+
+                } else {
+                    document.getElementById("account-layout").style.display = "none";
+                    document.getElementById("seller-profile-not-layout").style.display = "block";
+
+                }
+
+            } else {
+                $(".breadcrumb").notify(json.message, "error");
+            }
+
+
+        } else {
+            $(".breadcrumb").notify("Network Error Please try again Later", "error");
+        }
+
+
+    } else {
+        $(".breadcrumb").notify("Network Error Please try again Later", "error");
+    }
+
+}
